5b3bf1cc1b42ef9aaba9ad2222368cb3584ad33d
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-02-18 15:31:33 +0900

    Add Character Script

---
+++ Assets/Project/Scripts/Character/Character.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+namespace FTG.Character
+{
+    public enum AnimationState
+    {
+        IDLE,
+        MOVE,
+    }
+    
+    public class Character : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _moveThreshold = 0.1f;
+        
+        // TODO: Remove
+        [SerializeField] private Transform _targetTest;
+        
+        private Transform _target;
+        
+        private AnimationState _animState;
+        
+        public Transform Target
+        {
+            get => _target;
+            set
+            {
+                _target = value;
+                LookAtTarget();
+            }
+        }
+        
+        public AnimationState AnimState
+        {
+            get => _animState;
+            set
+            {
+                if (_animState == value) return;
+                
+                _animState = value;
+                switch (_animState)
+                {
+                    case AnimationState.IDLE:
+                        SkeletonAnim.AnimationName = "Idle";
+                        break;
+                    case AnimationState.MOVE:
+                        SkeletonAnim.AnimationName = "Run";
+                        break;
+                }
+                SkeletonAnim.loop = true;
+                
+                Debug.Log($"Animation: {_animState}");
+            }
+        }
+        
+        public SkeletonAnimation SkeletonAnim { get; private set; }
+
+#region Mono
+        private void Awake()
+        {
+            SkeletonAnim = GetComponent<SkeletonAnimation>();
+            Target       = _targetTest;
+            _animState   = AnimationState.IDLE;
+        }
+        
+        private void Update()
+        {
+            SetAnimation();
+            MoveLerp();
+        }
+#endregion
+
+        private void SetAnimation()
+        {
+            if (_target == null) return;
+            
+            if (Vector3.Distance(transform.position, _target.position) > _moveThreshold)
+            {
+                AnimState = AnimationState.MOVE;
+            }
+            else
+            {
+                AnimState = AnimationState.IDLE;
+            }
+        }
+        
+        private void MoveLerp()
+        {
+            if (_target == null) return;
+            
+            transform.position = Vector3.Lerp(transform.position, _target.position, _speed * Time.deltaTime);
+        }
+        
+        private void LookAtTarget()
+        {
+            if (_target == null) return;
+            
+            Vector2 dir = _target.transform.position - transform.position;
+            SkeletonAnim.Skeleton.ScaleX = dir.x > 0 ? -1 : 1;
+        }
+    }
+}
